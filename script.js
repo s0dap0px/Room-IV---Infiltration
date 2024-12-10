316773a5ccbe59047a8e432d47650455511e94f5
s0dap0px <160069774+s0dap0px@users.noreply.github.com>
2024-12-10 14:01:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-const images = [
-  "Images/Cutscene.png",
-  "Images/Cutscene0.png",
-  "Images/Cutscene1.png",
-  "Images/Cutscene1Scene2.png",
-  "Images/Cutscene1Scene3.png",
-  "Images/TitleScreen1.png",
-  "Images/TitleScreen2.png",
-];
+let timeRemaining = 420;
+let timerDisplay = document.getElementById("seconds");
 
-let currentIndex = 0;
+function beginTimer() {
+  let timerInterval = setInterval(function () {
+    timerDisplay.innerHTML = timeRemaining + "s";
 
-function changeBackgroundImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  document.body.style.backgroundImage = `url(${images[currentIndex]})`;
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      timerDisplay.innerHTML = "Game Over";
+      console.log("Timer has Reached 0");
+    } else {
+      timeRemaining--;
+    }
+  }, 1000);
+}
+beginTimer();
 
-  if (images[currentIndex] === "Images/TitleScreen2.png") {
-    roomChange();
+function TimerExpired() {
+  console.log("spooky");
+}
+
+// Modal 1
+function showScreen() {
+  document.getElementById("modal").style.display = "flex";
+}
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+function checkAnswer1() {
+  if (document.getElementById("answer-river").value == "river") {
+    console.log("Correct!");
+    // The correct answer remains unchanged on the textbox, wrong answer erases.
   }
 }
 
-function roomChange() {
-  window.location.href = "https://rjm7qp.csb.app/"; // Redirect to URL
+// Modal 2
+function showScreen2() {
+  document.getElementById("modal2").style.display = "flex";
+}
+function closeModal2() {
+  document.getElementById("modal2").style.display = "none";
 }
 
-document.body.addEventListener("click", changeBackgroundImage);
-
-// music WIP
-const backgroundMusic = new Audio("audio/.mp3");
-backgroundMusic.loop = true;
-
-const playbackState = getCookie("musicPlaybackState");
-if (playbackState === "playing") {
-  const currentTime = parseFloat(getCookie("musicCurrentTime"));
-  backgroundMusic.currentTime = currentTime;
-  backgroundMusic.play();
-} else {
-  backgroundMusic.play();
+// Modal 3
+function showScreen3() {
+  document.getElementById("modal3").style.display = "flex";
 }
+function closeModal3() {
+  document.getElementById("modal3").style.display = "none";
+}
+
+var timeLeft = 900;
+
+window.onload = function () {
+  var demoElement = document.getElementById("demo");
+};
